d4c39b993d26265fe4e8f380e3a9c3183c47d7c1
Cigomba Miler <Cigombamiler@gmail.com>
2022-04-13 16:40:25 -0700

    fixed case name=='' @ login redirect. Stable version

--- website/main.py
+++ website/main.py
@@ -14,8 +14,10 @@ app.secret_key = "TryToGuessMe"
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == 'POST':
-        print(request.form)
-        session[NAME_KEY]: request.form["name"]
+        # print(request.form)
+        session[NAME_KEY] = request.form["name"]
+        return redirect(url_for("home"))
+    elif request.method == 'GET' and NAME_KEY in session:
         return redirect(url_for("home"))
     return render_template("/login.html")
 
@@ -25,12 +27,16 @@ def login():
 def home():
     if NAME_KEY not in session:
         return redirect(url_for("login"))
+    elif session[NAME_KEY] == '':
+        session.pop(NAME_KEY, None)
+        return redirect(url_for("login"))
     name = session[NAME_KEY]
     return render_template("index.html")
 
 
 @app.route("/logout")
 def logout():
+    print(session, 'session:')
     session.pop(NAME_KEY, None)
     return render_template("/logout.html")
 
